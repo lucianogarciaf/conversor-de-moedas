fdee8bbe16f828a065ed8244f86a73d34c04794f
Luciano <lucluci@hotmail.com>
2025-08-06 13:05:37 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http; // conjunto de imports necessarios
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+const request = "https://api.fxratesapi.com/latest"; //endereço da api
+
+void main() async {
+  runApp(MaterialApp(home: Home(), debugShowCheckedModeBanner: false));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+
+  return json.decode(response.body);
+}
